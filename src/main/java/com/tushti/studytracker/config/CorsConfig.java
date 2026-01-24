a0b68b70d1eaa8ed6025ee8a808fd752af622866
Tushti Patel <tushtipatel1015@gmail.com>
2026-01-23 23:34:52 -0500

    added global CORS config

---
+++ src/main/java/com/tushti/studytracker/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.tushti.studytracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+          .allowedOrigins(
+            "http://localhost:5173",
+            "https://study-tracker-nine-mu.vercel.app"
+          )
+          .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+          .allowedHeaders("*");
+      }
+    };
+  }
+}
