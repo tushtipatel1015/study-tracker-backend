37e84fedc4de287beb723d692002c9f57da27480
Tushti Patel <tushtipatel1015@gmail.com>
2026-01-23 23:43:53 -0500

    wip

--- src/main/java/com/tushti/studytracker/TaskController.java
+++ src/main/java/com/tushti/studytracker/TaskController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/tasks")
 @CrossOrigin(origins = {
-    "http://localhost:5173",
+    //"http://localhost:5173",
     "https://study-tracker-nine-mu.vercel.app/"
 })
 
