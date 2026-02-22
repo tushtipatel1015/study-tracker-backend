df9539db189e58a42e37f2a20ebf7751ad23de99
Tushti Patel <tushtipatel1015@gmail.com>
2026-02-22 15:02:16 -0500

    wip

--- src/main/java/com/tushti/studytracker/Task.java
+++ src/main/java/com/tushti/studytracker/Task.java
@@ -13,7 +13,7 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
+    @Column(name = "user_id")
     private String userId;
 
     @Column(name = "order_index")
